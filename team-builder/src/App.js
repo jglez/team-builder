6dbef85e274c01b1ff37dc503d139ae93d37a96c
Jorge Gonzalez <jglez.cs@gmail.com>
2020-12-15 18:12:19 -0600

    Completed form with 3 inputs and a submit button

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,29 +1,10 @@
-import React, { useState } from 'react'
 import './App.css';
-
-const teamMembers = [
-  { name: 'Elon Musk', role: 'product design engineer' },
-  { name: 'George Hotz', role: 'cybersecurity engineer' },
-  { name: 'John Carmack', role: 'software engineer' }
-]
-
-function Form() {
-  const [members, setMembers] = useState(teamMembers)
-  console.log(members)
-
-  return null
-}
-
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
       <h1>Team Members:</h1>
-
-      {teamMembers.map((member, index) => {
-        return <div key={index}>{member.name} is a {member.role}</div>
-      })}
-
       <Form />
     </div>
   );
